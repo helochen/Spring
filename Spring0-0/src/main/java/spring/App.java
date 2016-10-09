5549f12e83439d1397ac48ba6d52d2182063fe5f
home <334972898@qq.com>
2016-10-10 00:34:23 +0800

    移除项目配置文件不知道对不对

--- Spring0-0/src/main/java/spring/App.java
+++ Spring0-0/src/main/java/spring/App.java
@@ -1,13 +1,26 @@
 package spring;
 
+import org.chen.Interface.Performer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	private static ApplicationContext ctx = null;
+	static{
+		ctx = new ClassPathXmlApplicationContext("spring.xml");
+	}
+	
+    public static void main( String[] args ) throws Exception
     {
+    	Performer p = (Performer) ctx.getBean("duke");
+    	p.perform();
         System.out.println( "Hello World!" );
+        
     }
 }
