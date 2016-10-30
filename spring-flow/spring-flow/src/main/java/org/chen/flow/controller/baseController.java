e5d897f29fed9240f6a722d20059382d44742d24
home <334972898@qq.com>
2016-10-30 12:53:57 +0800

    又他妈出错了

---
+++ spring-flow/spring-flow/src/main/java/org/chen/flow/controller/baseController.java
@@ -0,0 +1,16 @@
+package org.chen.flow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class baseController {
+
+	@RequestMapping("{page}")
+	public String getPage(@PathVariable String page) {
+		System.out.println(page);
+		return "views/" + page;
+	}
+}
